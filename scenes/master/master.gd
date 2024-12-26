7780e7df357dea11115f8ff668efcf8021a00df1
Niamh <niamh.linguist@gmail.com>
2024-12-26 15:05:50 +0200

    sound manager

--- scenes/master/master.gd
+++ scenes/master/master.gd
@@ -2,9 +2,10 @@ extends CanvasLayer
 
 @onready var main: Control = $Main
 @onready var game: Control = $Game
+@onready var sound: AudioStreamPlayer = $Sound
 
 func _ready() -> void:
-	show_game(false)
+	on_game_exit_pressed()
 	SignalManager.on_game_exit_pressed.connect(on_game_exit_pressed)
 	SignalManager.on_lvl_selected.connect(on_lvl_selected)
 
@@ -14,6 +15,8 @@ func show_game(show: bool) -> void:
 
 func on_game_exit_pressed() -> void:
 	show_game(false)
+	SoundManager.play_sound(sound, SoundManager.SOUND_MAIN_MENU)
 
 func on_lvl_selected(lvl_num: int) -> void:
 	show_game(true)
+	SoundManager.play_sound(sound, SoundManager.SOUND_IN_GAME)
